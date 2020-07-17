646f8a584469d1ccd8e36a7457cdb7ef32346ad3
yanghb5 <yanghb5@lenovo.com>
2020-07-17 15:39:41 +0800

    upd: add mifify

---
+++ templates/src/index.js
@@ -0,0 +1,5 @@
+import './index.css';
+
+const wrapper = document.querySelectorAll('.wrapper')[0];
+const value = 'Cousin';
+wrapper.innerHTML = `Hello, <span>${value}</span>, Welcome to use the cousin scaffold!`;
---
+++ templates/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './index.css';
+
+class App extends React.PureComponent {
+  render(): React.ReactNode {
+    return (
+      <div>
+        Hello, <span>Cousin</span>, Welcome to use the cousin scaffold!
+      </div>
+    );
+  }
+}
+
+ReactDom.render(<App />, document.querySelectorAll('.wrapper')[0]);
