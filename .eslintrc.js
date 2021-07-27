9a3d7ef5f37909450198f977106da968cc4c4ff6
Yang Bruce <yanghongbo@YangdeMacBook-Pro.local>
2021-07-27 18:30:16 +0800

    feat: add dependencies and modify eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,18 @@
 module.exports = {
-  extends: [
-    'standard',
-    'plugin:prettier/recommended',
-    'prettier/standard',
-    'plugin:json/recommended',
-  ],
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: ['standard', 'prettier'],
+  plugins: ['prettier', 'json'],
   rules: {
     'prettier/prettier': 'warn',
   },
